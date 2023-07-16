5956d131e853b72ef8b7269e1cf83cd8badd2323
potatoboiler <61665896+potatoboiler@users.noreply.github.com>
2023-07-16 12:23:30 -0700

    Update fetch workflow and add new champs

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -7,7 +7,13 @@ const octokit = new Octokit({
     auth: process.env.GH_PAT,
 });
 
-async function main() {
+const getTreeFromEntry = (entry) => octokit.rest.git.getTree({
+    owner: "InFinity54",
+    repo: "LoL_DDragon",
+    tree_sha: entry.sha
+});
+
+async function fetch_images() {
     const master = await octokit.rest.git.getRef({
         owner: "InFinity54",
         repo: "LoL_DDragon",
@@ -24,24 +30,11 @@ async function main() {
             tree_sha: commit.data.commit.tree.sha,
         }))
         .then((tree) => tree.data.tree.filter((key) => key.path === "latest")[0])
-        .then((entry) => octokit.rest.git.getTree({
-            owner: "InFinity54",
-            repo: "LoL_DDragon",
-            tree_sha: entry.sha
-        }))
+        .then(getTreeFromEntry)
         .then((tree) => tree.data.tree.filter((key) => key.path === "img")[0])
-        .then((entry) => octokit.rest.git.getTree({
-            owner: "InFinity54",
-            repo: "LoL_DDragon",
-            tree_sha: entry.sha
-        }))
+        .then(getTreeFromEntry)
         .then((tree) => tree.data.tree.filter((key) => key.path === "champion")[0])
-        .then((entry) => octokit.rest.git.getTree({
-            owner: "InFinity54",
-            repo: "LoL_DDragon",
-            tree_sha: entry.sha,
-            recursive: "true"
-        }))
+        .then(getTreeFromEntry)
         .then(async (tree) => await Promise.all(tree.data.tree.map(async (entry) => [entry.path, await octokit.rest.git.getBlob({
             owner: "InFinity54",
             repo: "LoL_DDragon",
@@ -66,4 +59,42 @@ async function main() {
     return master;
 }
 
-main()
+async function fetch_names() {
+    const master = await octokit.rest.git.getRef({
+        owner: "InFinity54",
+        repo: "LoL_DDragon",
+        ref: "heads/master",
+    })
+        .then((ref) => octokit.rest.repos.getCommit({
+            owner: "InFinity54",
+            repo: "LoL_DDragon",
+            ref: ref.data.object.sha,
+        }))
+        .then((commit) => octokit.rest.git.getTree({
+            owner: "InFinity54",
+            repo: "LoL_DDragon",
+            tree_sha: commit.data.commit.tree.sha,
+        }))
+        .then((tree) => tree.data.tree.filter((key) => key.path === "latest")[0])
+        .then(getTreeFromEntry)
+        .then((tree) => tree.data.tree.filter((key) => key.path === "data")[0])
+        .then(getTreeFromEntry)
+        .then((tree) => tree.data.tree.filter((key) => key.path === "en_US")[0])
+        .then(getTreeFromEntry)
+        .then((tree) => tree.data.tree.filter((key) => key.path === "champion.json")[0])
+        .then((entry) => octokit.rest.git.getBlob({
+            owner: "Infinity54",
+            repo: "LoL_DDragon",
+            file_sha: entry.sha,
+        }))
+        .then((entry) => {
+            const champs = JSON.parse(atob(entry.data.content)).data
+            Object.keys(champs).forEach((champId) => champs[champId] = champs[champId].name)
+            return champs
+        })
+
+    console.log(master)
+}
+
+// fetch_images()
+fetch_names()
--- src/App.js
+++ src/App.js
@@ -1,171 +1,173 @@
 import './App.css'
 import React from 'react'
 
-const NUM_CHAMPS = 161
-
-const CHAMP_LIST = [
-  'Aatrox',
-  'Ahri',
-  'Akali',
-  'Akshan',
-  'Alistar',
-  'Amumu',
-  'Anivia',
-  'Annie',
-  'Aphelios',
-  'Ashe',
-  'Aurelion_Sol',
-  'Azir',
-  'Bard',
-  'Bel\'Veth',
-  'Blitzcrank',
-  'Brand',
-  'Braum',
-  'Caitlyn',
-  'Camille',
-  'Cassiopeia',
-  'Cho\'Gath',
-  'Corki',
-  'Darius',
-  'Diana',
-  'Dr._Mundo',
-  'Draven',
-  'Ekko',
-  'Elise',
-  'Evelynn',
-  'Ezreal',
-  'Fiddlesticks',
-  'Fiora',
-  'Fizz',
-  'Galio',
-  'Gangplank',
-  'Garen',
-  'Gnar',
-  'Gragas',
-  'Graves',
-  'Gwen',
-  'Hecarim',
-  'Heimerdinger',
-  'Illaoi',
-  'Irelia',
-  'Ivern',
-  'Janna',
-  'Jarvan_IV',
-  'Jax',
-  'Jayce',
-  'Jhin',
-  'Jinx',
-  'Kai\'Sa',
-  'Kalista',
-  'Karma',
-  'Karthus',
-  'Kassadin',
-  'Katarina',
-  'Kayle',
-  'Kayn',
-  'Kennen',
-  'Kha\'Zix',
-  'Kindred',
-  'Kled',
-  'Kog\'Maw',
-  'LeBlanc',
-  'Lee_Sin',
-  'Leona',
-  'Lillia',
-  'Lissandra',
-  'Lucian',
-  'Lulu',
-  'Lux',
-  'Malphite',
-  'Malzahar',
-  'Maokai',
-  'Master_Yi',
-  'Miss_Fortune',
-  'Mordekaiser',
-  'Morgana',
-  'Nami',
-  'Nasus',
-  'Nautilus',
-  'Neeko',
-  'Nidalee',
-  'Nilah',
-  'Nocturne',
-  'Nunu',
-  'Olaf',
-  'Orianna',
-  'Ornn',
-  'Pantheon',
-  'Poppy',
-  'Pyke',
-  'Qiyana',
-  'Quinn',
-  'Rakan',
-  'Rammus',
-  'Rek\'Sai',
-  'Rell',
-  'Renata_Glasc',
-  'Renekton',
-  'Rengar',
-  'Riven',
-  'Rumble',
-  'Ryze',
-  'Samira',
-  'Sejuani',
-  'Senna',
-  'Seraphine',
-  'Sett',
-  'Shaco',
-  'Shen',
-  'Shyvana',
-  'Singed',
-  'Sion',
-  'Sivir',
-  'Skarner',
-  'Sona',
-  'Soraka',
-  'Swain',
-  'Sylas',
-  'Syndra',
-  'Tahm_Kench',
-  'Taliyah',
-  'Talon',
-  'Taric',
-  'Teemo',
-  'Thresh',
-  'Tristana',
-  'Trundle',
-  'Tryndamere',
-  'Twisted_Fate',
-  'Twitch',
-  'Udyr',
-  'Urgot',
-  'Varus',
-  'Vayne',
-  'Veigar',
-  'Vel\'Koz',
-  'Vex',
-  'Viego',
-  'Viktor',
-  'Vi',
-  'Vladimir',
-  'Volibear',
-  'Warwick',
-  'Wukong',
-  'Xayah',
-  'Xerath',
-  'Xin_Zhao',
-  'Yasuo',
-  'Yone',
-  'Yorick',
-  'Yuumi',
-  'Zac',
-  'Zed',
-  'Zeri',
-  'Ziggs',
-  'Zilean',
-  'Zoe',
-  'Zyra'
-]
+const CHAMPS = {
+  Aatrox: 'Aatrox',
+  Ahri: 'Ahri',
+  Akali: 'Akali',
+  Akshan: 'Akshan',
+  Alistar: 'Alistar',
+  Amumu: 'Amumu',
+  Anivia: 'Anivia',
+  Annie: 'Annie',
+  Aphelios: 'Aphelios',
+  Ashe: 'Ashe',
+  AurelionSol: 'Aurelion Sol',
+  Azir: 'Azir',
+  Bard: 'Bard',
+  Belveth: "Bel'Veth",
+  Blitzcrank: 'Blitzcrank',
+  Brand: 'Brand',
+  Braum: 'Braum',
+  Caitlyn: 'Caitlyn',
+  Camille: 'Camille',
+  Cassiopeia: 'Cassiopeia',
+  Chogath: "Cho'Gath",
+  Corki: 'Corki',
+  Darius: 'Darius',
+  Diana: 'Diana',
+  Draven: 'Draven',
+  DrMundo: 'Dr. Mundo',
+  Ekko: 'Ekko',
+  Elise: 'Elise',
+  Evelynn: 'Evelynn',
+  Ezreal: 'Ezreal',
+  Fiddlesticks: 'Fiddlesticks',
+  Fiora: 'Fiora',
+  Fizz: 'Fizz',
+  Galio: 'Galio',
+  Gangplank: 'Gangplank',
+  Garen: 'Garen',
+  Gnar: 'Gnar',
+  Gragas: 'Gragas',
+  Graves: 'Graves',
+  Gwen: 'Gwen',
+  Hecarim: 'Hecarim',
+  Heimerdinger: 'Heimerdinger',
+  Illaoi: 'Illaoi',
+  Irelia: 'Irelia',
+  Ivern: 'Ivern',
+  Janna: 'Janna',
+  JarvanIV: 'Jarvan IV',
+  Jax: 'Jax',
+  Jayce: 'Jayce',
+  Jhin: 'Jhin',
+  Jinx: 'Jinx',
+  Kaisa: "Kai'Sa",
+  Kalista: 'Kalista',
+  Karma: 'Karma',
+  Karthus: 'Karthus',
+  Kassadin: 'Kassadin',
+  Katarina: 'Katarina',
+  Kayle: 'Kayle',
+  Kayn: 'Kayn',
+  Kennen: 'Kennen',
+  Khazix: "Kha'Zix",
+  Kindred: 'Kindred',
+  Kled: 'Kled',
+  KogMaw: "Kog'Maw",
+  KSante: "K'Sante",
+  Leblanc: 'LeBlanc',
+  LeeSin: 'Lee Sin',
+  Leona: 'Leona',
+  Lillia: 'Lillia',
+  Lissandra: 'Lissandra',
+  Lucian: 'Lucian',
+  Lulu: 'Lulu',
+  Lux: 'Lux',
+  Malphite: 'Malphite',
+  Malzahar: 'Malzahar',
+  Maokai: 'Maokai',
+  MasterYi: 'Master Yi',
+  Milio: 'Milio',
+  MissFortune: 'Miss Fortune',
+  MonkeyKing: 'Wukong',
+  Mordekaiser: 'Mordekaiser',
+  Morgana: 'Morgana',
+  Nami: 'Nami',
+  Nasus: 'Nasus',
+  Nautilus: 'Nautilus',
+  Neeko: 'Neeko',
+  Nidalee: 'Nidalee',
+  Nilah: 'Nilah',
+  Nocturne: 'Nocturne',
+  Nunu: 'Nunu & Willump',
+  Olaf: 'Olaf',
+  Orianna: 'Orianna',
+  Ornn: 'Ornn',
+  Pantheon: 'Pantheon',
+  Poppy: 'Poppy',
+  Pyke: 'Pyke',
+  Qiyana: 'Qiyana',
+  Quinn: 'Quinn',
+  Rakan: 'Rakan',
+  Rammus: 'Rammus',
+  RekSai: "Rek'Sai",
+  Rell: 'Rell',
+  Renata: 'Renata Glasc',
+  Renekton: 'Renekton',
+  Rengar: 'Rengar',
+  Riven: 'Riven',
+  Rumble: 'Rumble',
+  Ryze: 'Ryze',
+  Samira: 'Samira',
+  Sejuani: 'Sejuani',
+  Senna: 'Senna',
+  Seraphine: 'Seraphine',
+  Sett: 'Sett',
+  Shaco: 'Shaco',
+  Shen: 'Shen',
+  Shyvana: 'Shyvana',
+  Singed: 'Singed',
+  Sion: 'Sion',
+  Sivir: 'Sivir',
+  Skarner: 'Skarner',
+  Sona: 'Sona',
+  Soraka: 'Soraka',
+  Swain: 'Swain',
+  Sylas: 'Sylas',
+  Syndra: 'Syndra',
+  TahmKench: 'Tahm Kench',
+  Taliyah: 'Taliyah',
+  Talon: 'Talon',
+  Taric: 'Taric',
+  Teemo: 'Teemo',
+  Thresh: 'Thresh',
+  Tristana: 'Tristana',
+  Trundle: 'Trundle',
+  Tryndamere: 'Tryndamere',
+  TwistedFate: 'Twisted Fate',
+  Twitch: 'Twitch',
+  Udyr: 'Udyr',
+  Urgot: 'Urgot',
+  Varus: 'Varus',
+  Vayne: 'Vayne',
+  Veigar: 'Veigar',
+  Velkoz: "Vel'Koz",
+  Vex: 'Vex',
+  Vi: 'Vi',
+  Viego: 'Viego',
+  Viktor: 'Viktor',
+  Vladimir: 'Vladimir',
+  Volibear: 'Volibear',
+  Warwick: 'Warwick',
+  Xayah: 'Xayah',
+  Xerath: 'Xerath',
+  XinZhao: 'Xin Zhao',
+  Yasuo: 'Yasuo',
+  Yone: 'Yone',
+  Yorick: 'Yorick',
+  Yuumi: 'Yuumi',
+  Zac: 'Zac',
+  Zed: 'Zed',
+  Zeri: 'Zeri',
+  Ziggs: 'Ziggs',
+  Zilean: 'Zilean',
+  Zoe: 'Zoe',
+  Zyra: 'Zyra'
+}
+const CHAMP_LIST = Object.keys(CHAMPS)
+const NUM_CHAMPS = CHAMP_LIST.length
 
 class Arena extends React.Component {
   constructor(props) {
@@ -184,13 +186,13 @@ class Arena extends React.Component {
         }}
       >
         <ChampColumn
-          champs={champs.slice(0, 15)}
+          champsIdx={champs.slice(0, 15)}
           n={this.state.n_players}
           id='left'
         />
         <div></div>
         <ChampColumn
-          champs={champs.slice(15, 30)}
+          champsIdx={champs.slice(15, 30)}
           n={this.state.n_players}
           id='right'
         />
@@ -206,12 +208,14 @@ class ChampColumn extends React.Component {
     var champ_idx = 0
     var rows = []
     for (var i = 0; i < this.props.n; i++) {
+      const chosenChampsIdx = this.props.champsIdx.slice(champ_idx, champ_idx + 3);
+      const champIds = chosenChampsIdx.map(champIdx => CHAMP_LIST[champIdx])
+      const champNames = chosenChampsIdx.map((champIdx) => CHAMPS[CHAMP_LIST[champIdx]])
       rows.push(
         <div style={{ height: '20vh' }}>
           <ChampTable
-            c={this.props.champs
-              .slice(champ_idx, champ_idx + 3)
-              .map(x => CHAMP_LIST[x])}
+            c={champIds}
+            cn={champNames}
             id={this.props.id}
           />
         </div>
@@ -227,9 +231,9 @@ function ChampTable(props) {
   let id = props.id
   return (
     <table id={id}>
-      <ChampEntry c={props.c[0]} id={id} />
-      <ChampEntry c={props.c[1]} id={id} />
-      <ChampEntry c={props.c[2]} id={id} />
+      <ChampEntry c={props.c[0]} cn={props.cn[0]} id={id} />
+      <ChampEntry c={props.c[1]} cn={props.cn[1]} id={id} />
+      <ChampEntry c={props.c[2]} cn={props.cn[2]} id={id} />
     </table>
   )
 }
@@ -238,15 +242,15 @@ function ChampEntry(props) {
   var entry =
     <img
       src={GetChampionIconURI(props.c)}
-      alt={props.c}
+      alt={props.cn}
       height='35'
     />;
-  entry = (props.id === 'left') ? <td>{props.c}{' '}{entry}</td> : <td>{entry}{' '}{props.c}</td>;
+  entry = (props.id === 'left') ? <td>{props.cn}{' '}{entry}</td> : <td>{entry}{' '}{props.cn}</td>;
   return <tr>{entry}</tr>
 }
 
 function GetChampionIconURI(name) {
-  return '/aram-drafter/ico/' + name + '_OriginalSquare.png'
+  return '/aram-drafter/ico/' + name + '.png'
 }
 
 function ShuffleArray() {
